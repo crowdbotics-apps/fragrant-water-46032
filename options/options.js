83142891b2ffa7667cf34be14c473843a6b5102c
Crowdbotics <team@crowdbotics.com>
2024-02-06 15:05:05 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fragrant-water-46032",
+  url: "https://fragrant-water-46032.botics.co",
+  api: "https://fragrant-water-46032.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
